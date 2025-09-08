5467c685a5cd6e2293c9f5ac014aede54f6959af
Simon Andrews <simon@proteo.me.uk>
2025-09-08 16:22:12 +0100

    Basic functionality for web front end

--- WebFrontEnd/www/static/js/main.js
+++ WebFrontEnd/www/static/js/main.js
@@ -1,5 +1,33 @@
 $( document ).ready(function() {
 
+    $("#runanalysis").click(run_analysis)
 })
 
 
+function run_analysis(){
+
+    alert("Running analysis")
+    // We need to collect the information from the form and then
+    // submit an ajax request 
+
+    
+    let user_data = {
+        "species": $("#species").val(),
+        "query" : $("#query").val(),
+        "background" : $("#background").val()
+    }
+
+    $.ajax(
+        {
+            url: "/runanalysis",
+            method: "POST",
+            data: user_data,
+            success: function(folder) {
+                window.location.replace(`./jobs/${folder}`)
+            },
+            error: function(message) {
+                alert("Failed to run analysis: "+message)
+            }
+        }
+    )
+}
--- WebFrontEnd/www/webapp.py
+++ WebFrontEnd/www/webapp.py
@@ -4,7 +4,8 @@ from flask import Flask, request, render_template, make_response
 import random
 from urllib.parse import quote_plus
 from pathlib import Path
-import json
+import string
+
 
 app = Flask(__name__)
 
@@ -23,22 +24,71 @@ def index():
     return render_template("index.html", data=data)
 
 
-def get_form():
-    # In addition to the main arguments we also add the session
-    # string from the cookie
-    session = ""
+@app.route("/runanalysis", methods=["POST"])
+def runanalysis():
 
-    if "groupactivity_session_id" in request.cookies:
-        session = request.cookies["groupactivity_session_id"]
+    # We'll have a form with species, query and background
+    # lists in it.  We'll make a directory for the data 
+    # and put this information into it.  We can then redirect
+    # to the output url and we're done.  Everything else will
+    # be picked up by the analysis code.
+
+    form = get_form()
+
+    id = generate_random_folder()
+
+    with open(id / "species.txt","wt", encoding="utf8") as out:
+        out.write(form["species"])
+
+    with open(id / "query_genes.txt","wt", encoding="utf8") as out:
+        out.write(form["query"])
+
+    with open(id / "background_genes.txt","wt", encoding="utf8") as out:
+        out.write(form["background"])
+
+
+    return id.name
+
+
+def generate_random_folder():
+
+    base_folder = Path(__file__).parent.parent / "Jobs"
+
+    attempts = 0
+    while True:
+        attempts += 1
+
+        id = ""
+        for _ in range(10):
+            id += string.ascii_lowercase.upper()[random.randint(0,25)]
+
+
+        if (base_folder / id).exists():
+            if attempts == 10:
+                id = ""
+                break
+            continue
+
+        else:
+            break
+
+    if id:
+        id = base_folder / id
+        id.mkdir()
+        return id
+    
+    raise Exception("Couldn't make new folder - failed 10 times!")
+
+
+
+def get_form():
 
     if request.method == "GET":
         form = request.args.to_dict(flat=True)
-        form["session"] = session
         return form
 
     elif request.method == "POST":
         form = request.form.to_dict(flat=True)
-        form["session"] = session
         return form
 
 
