af22e252d73be3f51894d6c34ccf164bd295eb7b
Simon Andrews <simon@proteo.me.uk>
2025-09-08 17:32:10 +0100

    Added script to do GeneSet Analysis

---
+++ AnalysisScripts/run_gene_set_analysis.R
@@ -0,0 +1,35 @@
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(org.Mm.eg.db)
+library(tidyverse)
+
+commandArgs(trailingOnly = TRUE)[1] -> job_id
+
+setwd(paste0("../WebFrontEnd/",job_id))
+
+read_tsv("species.txt", col_names="species") |> pull(species) -> species
+
+read_tsv("query_genes.txt",col_names="query") |>
+    pull(query) -> query
+
+read_tsv("background_genes.txt",col_names="background") |>
+    pull(background) -> background
+
+case_when(
+    str_detect("Human",species) ~ org.Hs.eg.db,
+    str_detect("Mouse",species) ~ org.Mm.eg.db,
+    TRUE ~ NULL
+) -> org_db
+
+enrichGO(
+    query,
+    OrgDb = org.Mm.eg.db,
+    keyType = "SYMBOL",
+    universe = background,
+    ont = "BP",
+    minGSSize = 10,
+    maxGSSize = 200,
+    readable = TRUE
+) -> enrichgo_results
+
+
