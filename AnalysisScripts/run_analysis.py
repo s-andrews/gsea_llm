e82c760965a977b15b7114f2337a6d3acf550306
Muna Yusuf <muna369241@hotmail.com>
2025-09-09 13:41:27 +0100

    edit run analysis to include ollamaGo.py

--- AnalysisScripts/run_analysis.py
+++ AnalysisScripts/run_analysis.py
@@ -20,10 +20,10 @@ def run_step(command, log_file, error_file, cwd=None):
     with open(log_file, "a") as out, open(error_file, "a") as err:
         result = subprocess.run(
             command,
-            stdout=out,
+            stdout=out,     # error check
             stderr=err,
-            cwd=cwd,
-            text=True
+            cwd=cwd,     # run command in the specifiv folder if provided
+            text=True      # ensures error messages are in text form
         )
     if result.returncode != 0:
         raise RuntimeError(f"Step failed: {' '.join(command)}")
@@ -44,26 +44,26 @@ def main():
     # r_cwd = project_root / "WebFrontEnd"
     try:
         # run gsea in r
-        print("Running GSEA..")
-        run_step(
-            ["Rscript", str(r_script_path), str(job_folder)],
-            log_file,
-            error_file,
-            cwd = project_root
-        )
+        # print("Running GSEA..")
+        # run_step(
+        #     ["Rscript", str(r_script_path), str(job_folder)],
+        #     log_file,
+        #     error_file,
+        #     cwd = project_root
+        # )
 
-        # extract GO text from GO ids
+        # # extract GO text from GO ids
 
-        go_script_path = project_root / "AnalysisScripts" / "go_to_description.py"
-        print("Extrcating GO text...")
-        run_step(
-            [sys.executable, str(go_script_path), job_id],
-            log_file,
-            error_file,
-            cwd = project_root
-        )
+        # go_script_path = project_root / "AnalysisScripts" / "go_to_description.py"
+        # print("Extrcating GO text...")
+        # run_step(
+        #     [sys.executable, str(go_script_path), job_id],
+        #     log_file,
+        #     error_file,
+        #     cwd = project_root
+        # )
 
-        run_llm_path = project_root / "AnalysisScripts" / "summarise_terms.py"
+        run_llm_path = project_root / "AnalysisScripts" / "ollamaGo.py"
 
         #run llm to summarise terms
         print("Running LLM....")
