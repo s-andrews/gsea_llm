a1c46db4b6777031d7e6d72f35b234c0db2e627b
Simon Andrews <simon@proteo.me.uk>
2025-09-09 10:34:35 +0100

    Fixes for R analysis

--- AnalysisScripts/run_gene_set_analysis.R
+++ AnalysisScripts/run_gene_set_analysis.R
@@ -15,21 +15,49 @@ read_tsv("query_genes.txt",col_names="query") |>
 read_tsv("background_genes.txt",col_names="background") |>
     pull(background) -> background
 
-case_when(
-    str_detect("Human",species) ~ org.Hs.eg.db,
-    str_detect("Mouse",species) ~ org.Mm.eg.db,
-    TRUE ~ NULL
-) -> org_db
+org_db <- NULL
 
-enrichGO(
-    query,
-    OrgDb = org.Mm.eg.db,
-    keyType = "SYMBOL",
-    universe = background,
-    ont = "BP",
-    minGSSize = 10,
-    maxGSSize = 200,
-    readable = TRUE
+if (str_detect("Human",species)) {
+    org_db = org.Hs.eg.db
+}
+
+if (str_detect("Mouse",species)) {
+    org_db = org.Mm.eg.db
+}
+
+if (len(background) == 0) {
+    enrichGO(
+        query,
+        OrgDb = org.Mm.eg.db,
+        keyType = "SYMBOL",
+        ont = "BP",
+        minGSSize = 10,
+        maxGSSize = 200,
+        readable = TRUE
 ) -> enrichgo_results
+
+}
+else {
+    enrichGO(
+        query,
+        OrgDb = org.Mm.eg.db,
+        keyType = "SYMBOL",
+        universe = background,
+        ont = "BP",
+        minGSSize = 10,
+        maxGSSize = 200,
+        readable = TRUE
+    ) -> enrichgo_results
+}
+
+enrichgo_results@result |>
+    as_tibble() |>
+    write_tsv("cluster_profiler_result.tsv")
+
+enrichgo_results@result |>
+    as_tibble() |>
+    select(ID) |>
+    write_tsv("go_ids.txt")
+
 
 
