1f9246c704473de79ba9e1750fccb7c51a09427c
Andrew Muir <136808588+muir-a@users.noreply.github.com>
2025-09-10 12:23:37 +0100

    adjusted prompts to make table more precise

--- AnalysisScripts/ollamaGO.py
+++ AnalysisScripts/ollamaGO.py
@@ -10,15 +10,15 @@ job_id = sys.argv[1]
 job_folder = Path(__file__).parent.parent / ("WebFrontEnd/Jobs/"+job_id)
 
 # For LINUX
-client = ollama.Client(verify=False)
+client = ollama.Client(verify=False, timeout=300)
 
 p_path = Path(job_folder) / "prompts.txt"
 
 try:
     my_path = p_path.resolve(strict=True)
 except FileNotFoundError:
-    print("default prompt used", flush=True)
-    prompt = "Provided after these instructions is a list of biological process descriptions. Analyse them to determine whether a unifying theme exists. If no unifying theme is present among the majority, return only this exact sentence: There is no unifying theme among the provided biological processes. If a unifying theme does exist, return a list of broad categories that these processes are involved in, ranked by the number of descriptions that fit into each category. In a following table, assign each category a row and and list all the GO IDs that contribute to that category in the adjacent column. In a final paragraph, maximum 100 words, hypothesise if there is a shared biological process or pathway that is common to all the biological process descriptions provided."
+    print("default prompt used")
+    prompt = "Provided after these instructions is a list of biological process descriptions. Analyse them to determine whether a unifying theme exists. If no unifying theme is present among the majority, return only this exact sentence: There is no unifying theme among the provided biological processes. If a unifying theme does exist, return a single list of broad categories that these processes are involved in, ranked by the number of descriptions that fit into each category, limit this list to a maximum of 10 items. Next, in a single table, in the first column put each of the identified categories, one category per row, and list all the GO IDs that contribute to that category in the adjacent column. In a final paragraph, maximum 100 words, hypothesise if there is a shared biological process or pathway that is common to all the biological process descriptions provided."
 else:
     with open(p_path, "r") as text_file1:
         prompt = text_file1.read()
@@ -38,3 +38,5 @@ response = client.chat(
 
 with open(job_folder /'LLMresponse.txt', 'w') as output:
     output.write(response.message.content)
+
+print(response.message.content)
